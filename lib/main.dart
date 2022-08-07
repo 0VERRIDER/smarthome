1f5e631ba309a7b6b3f343edc1fb0df73ab8abb9
Anshil P <anshilp@karunya.edu.in>
2022-08-07 15:40:58 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Deo Home Page'),
+      home: const MyHomePage(title: 'Flutter DemRemoveo Home Page'),
     );
   }
 }
